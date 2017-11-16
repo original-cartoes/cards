36dd85a014f7532c536c4bbbd81abe915b134beb
Erick Wendel <erickwendel@Ericks-MacBook-Pro.local>
2017-11-16 12:09:51 -0200

    card detail

--- app/app.js
+++ app/app.js
@@ -2,11 +2,11 @@ angular
   .module('app', ['cardDetail', 'cardList'])
   .controller('appController', function(CardDetailService, CardListService) {
     this.name = CardDetailService.name;
-    // CardDetailService.detail().then(function(items) {
-    //   debugger;
-    // });
-
-    CardListService.list().then(function(items) {
+    CardDetailService.detail().then(function(items) {
       debugger;
     });
+
+    // CardListService.list().then(function(items) {
+    //   debugger;
+    // });
   });
