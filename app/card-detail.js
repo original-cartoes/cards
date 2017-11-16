6ba2df7369e49564a784724e9be15f682713ca60
Erick Wendel <erickwendel@Ericks-MacBook-Pro.local>
2017-11-16 12:05:56 -0200

    api

--- app/card-detail.js
+++ app/card-detail.js
@@ -4,13 +4,8 @@ angular.module('cardDetail', []).service('CardDetailService', function($http) {
     detail: function() {
       var req = {
         method: 'GET',
-        url: 'https://sandbox.original.com.br/cards/v1/0001',
-        headers: {
-          AEEE: 'aeee',
-          Authorization:
-            'Bearer MzFlYWE0NjAtY2FiMi0xMWU3LWE3YzAtMDA1MDU2OWE3MzA1OmV5SmhiR2NpT2lKSVV6STFOaUlzSW5SNWNDSTZJa3BYVkNKOS5leUowZVhCbElqb2lUMEYxZEdnaUxDSnBZWFFpT2pFMU1UQTRNalV6TnpRc0ltVjRjQ0k2TVRVeE1USTFOek0zTkN3aVlYVmtJam9pWWpZMk1HRTNaRFFpTENKcGMzTWlPaUphZFhBdWJXVWdSMkYwWlhkaGVTSXNJbk4xWWlJNklqTXhaV0ZoTkRZd0xXTmhZakl0TVRGbE55MWhOMk13TFRBd05UQTFOamxoTnpNd05TSXNJbXAwYVNJNklqZzBaamhpTWpVd0xXTmhZakl0TVRGbE55MWhOMk13TFdNeE56WXlaRFl3TlRRME5pSjkuMEVOemdmWlBLcEpTOEUxc05GeTdwWXd5MTRraUVEMi1yaUtQenNSUFVtZw==',
-          'developer-key': '28f9b5c9063a29601340f1a9a00507569ad7fac4',
-        },
+        url:
+          'https://cards-original.herokuapp.com?url=https://sandbox.original.com.br/cards/v1/0001',
       };
       return $http(req)
         .then(function(items) {
