b11705e1467450e84f42c24e9f116173c5cd271c
Asakawa <glaa@gft.com>
2017-11-16 12:15:21 -0200

    api

--- app/app.js
+++ app/app.js
@@ -1,12 +1,16 @@
 angular
-  .module('app', ['cardDetail', 'cardList'])
-  .controller('appController', function(CardDetailService, CardListService) {
+  .module('app', ['cardDetail', 'cardList', 'invoiceHistory'])
+  .controller('appController', function(CardDetailService, CardListService, InvoiceHistoryService) {
     this.name = CardDetailService.name;
     CardDetailService.detail().then(function(items) {
       debugger;
     });
 
     CardListService.list().then(function(items) {
+      debugger;
+    });
+
+    InvoiceHistoryService.listHistory().then(function(items){
       debugger;
     });
   });
--- app/invoice-history.js
+++ app/invoice-history.js
@@ -1,1 +1,19 @@
 //Gabriel
+angular.module('invoiceHistory', []).service('InvoiceHistoryService', function($http) {
+  return {
+    listHistory: function() {
+      var req = {
+        method: 'GET',
+        url:
+          'http://localhost:3000?url=https://sandbox.original.com.br/cards/v1/0001/invoices/history',
+      };
+      return $http(req)
+        .then(function(items) {
+          debugger;
+        })
+        .catch(error => {
+          throw new Error(error);
+        });
+    },
+  };
+});
